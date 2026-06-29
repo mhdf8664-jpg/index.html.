bbff5eea2d14985af0a7aa8ab33faefc964d5833
mhdf8664-jpg <mhdf8664@gmail.com>
2026-06-29 07:59:16 +0700

    Add functionality to open gift on button click

---
+++ script.js
@@ -0,0 +1,8 @@
+const openBtn = document.getElementById("openBtn");
+const gift = document.getElementById("gift");
+const opening = document.querySelector(".opening");
+
+openBtn.addEventListener("click", () => {
+    opening.style.display = "none";
+    gift.style.display = "block";
+});
